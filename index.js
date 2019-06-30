09018e65fe9c41a7f87e3e5f93c23dd4caf2992e
pi3rrot <4dakto@gmail.com>
2019-06-30 18:13:51 +0900

    update

--- index.js
+++ index.js
@@ -6,13 +6,6 @@ const isArrayLike = list => {
   const length = getLength(list);
   return typeof length == "number" && length >= 0 && length <= MAX_ARRAY_INDEX;
 };
-
-_.identity = v => v;
-_.idtt = _.identity;
-_.array = () => [];
-_.push_to = (val, obj) => (obj.push(val), val);
-_.noop = () => {};
-
 const bloop = (new_data, body) => (data, iteratee) => {
   const result = new_data(data);
   if (isArrayLike(data)) {
@@ -20,51 +13,25 @@ const bloop = (new_data, body) => (data, iteratee) => {
       body(iteratee(data[i], i, data), result);
     }
   } else {
-    for (const key in data) {
-      if (data.hasOwnProperty(key)) {
-        body(iteratee(data[key], key, data), result);
-      }
+    for (let i = 0, keys = _.keys(data), len = keys.length; i < len; i++) {
+      body(iteratee(data[keys[i]], keys[i], data), result);
     }
   }
   return result;
 };
-/**
- * {}, [], arguments, ArrayLike 를 인자로 받는 함수
- */
-// _.map = function(data, iteratee) {
-//   const new_list = [];
-//   if (isArrayLike(data)) {
-//     for (let i = 0, len = data.length; i < len; i++) {
-//       new_list.push(iteratee(data[i], i, data));
-//     }
-//   } else {
-//     for (const key in data) {
-//       if (data.hasOwnProperty(key)) {
-//         new_list.push(iteratee(data[key], key, data));
-//       }
-//     }
-//   }
-//   return new_list;
-// };
 
-// _.each = function(data, iteratee) {
-//   if (isArrayLike(data)) {
-//     for (let i = 0, len = data.length; i < len; i++) {
-//       iteratee(data[i], i, data);
-//     }
-//   } else {
-//     for (const key in data) {
-//       if (data.hasOwnProperty(key)) {
-//         iteratee(data[key], key, data);
-//       }
-//     }
-//   }
-//   return data;
-// };
-
-
+_.identity = v => v;
+_.idtt = _.identity;
+_.array = () => [];
+_.push_to = (val, obj) => (obj.push(val), val);
+_.noop = () => {};
+_.isObject = (obj) => {
+  const type = typeof obj;
+  return type === 'function' || type === 'object' && obj;
+};
+_.keys = (obj) => _.isObject(obj) ? Object.keys(obj) : [];
 _.map = bloop(_.array, _.push_to);
 _.each = bloop(_.idtt, _.noop);
-_.values = function(list) {
-  return _.map(list, _.identity);
-};
+_.values = (list) => _.map(list, _.identity);
+
+
