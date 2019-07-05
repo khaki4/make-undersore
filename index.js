a179bb1a5aa3617222e96e46e359bcfc26a14aaa
pi3rrot <4dakto@gmail.com>
2019-07-06 01:12:46 +0900

    add _.some, _.every

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 const { _, bloop } = require("./lib.js");
 
-_.find = bloop(_.noop, _.rester(_.idtt, 2), _.idtt);
-_.findIdx = bloop(_.constant(-1), _.rester(_.idtt, 3), _.idtt);
-_.findKey = bloop(_.noop, _.rester(_.idtt, 3), _.idtt);
-
-const user = { id: 4, name: "kevin" };
-console.log(_.findKey(user, v => typeof v === "string"));
+_.some = bloop(_.constant(false), _.constant(true), _.idtt);
+_.every = bloop(_.constant(true), _.constant(false), _.not);
