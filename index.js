d6a1e41a8ab84967e402e103dea260dc7bd9810a
pi3rrot <4dakto@gmail.com>
2019-07-06 01:05:49 +0900

    fix bloop for _.findKey

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const { _, bloop } = require('./lib.js');
+const { _, bloop } = require("./lib.js");
 
 _.find = bloop(_.noop, _.rester(_.idtt, 2), _.idtt);
 _.findIdx = bloop(_.constant(-1), _.rester(_.idtt, 3), _.idtt);
 _.findKey = bloop(_.noop, _.rester(_.idtt, 3), _.idtt);
 
-const user = [{ age: 25 }, { age: 55 }, { age: 57 }]
-console.log(_.findKey(user, (v) => {
-  return v.age >= 25;
-}))
+const user = { id: 4, name: "kevin" };
+console.log(_.findKey(user, v => typeof v === "string"));
--- lib.js
+++ lib.js
@@ -7,25 +7,24 @@ const isArrayLike = list => {
   return typeof length == "number" && length >= 0 && length <= MAX_ARRAY_INDEX;
 };
 
-const bloop = (new_data, body, stopper) =>
-  (data, iter_predi) => {
-    const result = new_data(data);
-    let memo;
-    if (isArrayLike(data)) {
-      for (let i = 0, len = data.length; i < len; i++) {
-        memo = iter_predi(data[i], i, data);
-        if (!stopper) body(memo, result, data[i], i);
-        else if (stopper(memo)) return body(memo, result, data[i], i);
-      }
-    } else {
-      for (let i = 0, keys = _.keys(data), len = keys.length; i < len; i++) {
-        memo = iter_predi(data[keys[i]], keys[i], data);
-        if (!stopper) body(memo, result, data[keys[i]], i);
-        else if (stopper(memo)) return body(memo, result, data[keys[i]], i);
-      }
+const bloop = (new_data, body, stopper) => (data, iter_predi) => {
+  const result = new_data(data);
+  let memo;
+  if (isArrayLike(data)) {
+    for (let i = 0, len = data.length; i < len; i++) {
+      memo = iter_predi(data[i], i, data);
+      if (!stopper) body(memo, result, data[i], i);
+      else if (stopper(memo)) return body(memo, result, data[i], i);
     }
-    return result;
-  };
+  } else {
+    for (let i = 0, keys = _.keys(data), len = keys.length; i < len; i++) {
+      memo = iter_predi(data[keys[i]], keys[i], data);
+      if (!stopper) body(memo, result, data[keys[i]], keys[i]);
+      else if (stopper(memo)) return body(memo, result, data[keys[i]], keys[i]);
+    }
+  }
+  return result;
+};
 
 _.identity = v => v;
 _.not = v => !v;
@@ -33,31 +32,31 @@ _.idtt = _.identity;
 _.array = () => [];
 _.push_to = (val, obj) => (obj.push(val), val);
 _.push = (obj, val) => (obj.push(val), obj);
-_.noop = () => {
-};
-_.negate = (func) => (...args) => !func(...args);
-_.isObject = (obj) => {
+_.noop = () => {};
+_.negate = func => (...args) => !func(...args);
+_.isObject = obj => {
   const type = typeof obj;
-  return type === 'function' || type === 'object' && obj;
+  return type === "function" || (type === "object" && obj);
 };
-_.keys = (obj) => _.isObject(obj) ? Object.keys(obj) : [];
+_.keys = obj => (_.isObject(obj) ? Object.keys(obj) : []);
 _.map = bloop(_.array, _.push_to);
 _.each = bloop(_.idtt, _.noop);
-_.values = (list) => _.map(list, _.identity);
+_.values = list => _.map(list, _.identity);
 _.rest = (list, num) => _.toArray(list).slice(num || 1);
 _.rester = (func, num) => (...args) => func.apply(null, _.rest(args, num));
-_.toArray = (list) => Array.isArray(list) ? list : _.values(list);
-_.if = (validator, func, alter) => (...args) => validator.apply(null, args) ? func(...args) : alter && alter(...args);
+_.toArray = list => (Array.isArray(list) ? list : _.values(list));
+_.if = (validator, func, alter) => (...args) =>
+  validator.apply(null, args) ? func(...args) : alter && alter(...args);
 _.toArray2 = _.if(Array.isArray, _.idtt, _.values);
-_.reverse = (list) => _.toArray(list).reverse();
+_.reverse = list => _.toArray(list).reverse();
 
 _.safety = _.with_validator = _.if;
-_.constant = (v) => () => v;
-_.isNumber = (a) => toString.call(a) === '[object Number]';
+_.constant = v => () => v;
+_.isNumber = a => toString.call(a) === "[object Number]";
 _.filter = bloop(_.array, _.if(_.idtt, _.rester(_.push)));
 _.reject = bloop(_.array, _.if(_.not, _.rester(_.push)));
 
 module.exports = {
   _,
-  bloop,
+  bloop
 };
